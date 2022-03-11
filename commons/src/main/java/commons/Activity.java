bbdbb9e330a98f8b45a1a723111b9ac65ceee88f
Macsim Violeta-Mara <V.M.Macsim@student.tudelft.nl>
2022-03-11 10:18:13 +0100

    Add activity class to commons

---
+++ commons/src/main/java/commons/Activity.java
@@ -0,0 +1,112 @@
+package commons;
+
+import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * Class that holds every info from the activity JSON.
+ */
+@Entity
+public class Activity {
+  @Id
+  @Column(name = "id", nullable = false)
+  private String id;
+  private String title;
+  private int consumption_in_wh;
+  private String image_path;
+
+  /**
+   * For object mappers.
+   */
+  public Activity() {
+  }
+
+  /**
+   * The constructor for the class.
+   *
+   * @param id          the activity id
+   * @param title       the title
+   * @param consumption_in_wh activity's consumption in watt
+   * @param path        the file path to the photo
+   */
+  public Activity(String id, String title, int consumption_in_wh, String path) {
+    this.id = id;
+    this.title = title;
+    this.consumption_in_wh = consumption_in_wh;
+    this.image_path = path;
+  }
+
+  /**
+   * A getter for the title.
+   *
+   * @return the activities title
+   */
+  public String getTitle() {
+    return title;
+  }
+
+  /**
+   * A getter for the path.
+   *
+   * @return the path to the photo
+   */
+  public String getImage_path() {
+    return image_path;
+  }
+
+  /**
+   * A getter for the consumption.
+   *
+   * @return an integer representing consumption
+   */
+  public int getConsumption_in_wh() {
+    return consumption_in_wh;
+  }
+
+  /**
+   * Getter for the id.
+   *
+   * @return a long generated using Identity
+   */
+  public String getId() {
+    return id;
+  }
+
+  /**
+   * A method that uses an API supportive version of the "equals" method.
+   *
+   * @param obj a random type of object to be compared to
+   * @return a boolean whether these 2 objects are the same or not
+   */
+  @Override
+  public boolean equals(Object obj) {
+    return EqualsBuilder.reflectionEquals(this, obj);
+  }
+
+  /**
+   * A method that uses an API supportive version of hashing.
+   *
+   * @return a hash code of the Player object
+   */
+  @Override
+  public int hashCode() {
+    return HashCodeBuilder.reflectionHashCode(this);
+  }
+
+  /**
+   * A method that uses an API supportive method of transforming data into a string.
+   *
+   * @return a string containing every detail about the player
+   */
+  @Override
+  public String toString() {
+    return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
+  }
+
+}
