00e39dd90553d15dc06bf4a4a2ffc30af867ef84
zliutkus <Liutkus@student.tudelft.nl>
2022-03-21 14:53:15 +0100

    Change question amounts in GameEntityController to 20

--- server/src/main/java/server/api/GameEntityController.java
+++ server/src/main/java/server/api/GameEntityController.java
@@ -163,7 +163,7 @@ public class GameEntityController {
    */
   @PostMapping(path = "/addPlayer")
   public ResponseEntity<GameEntity> addPlayerToGame(@RequestBody Player player) {
-    final int questionAmount = 4; // TODO: change amount to 20
+    final int questionAmount = 20;
     List<GameEntity> status = repo.findByStatus("WAITING");
     List<GameEntity> type = repo.findByType(GameEntity.Type.MULTIPLAYER);
     List<GameEntity> list = status.stream().filter(type::contains).collect(Collectors.toList());
