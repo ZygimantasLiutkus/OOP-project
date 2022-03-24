610e9982c5500f4edb55956fb4a79f21ea171f93
Adelina Cazacu <a.a.cazacu-1@student.tudelft.nl>
2022-03-24 10:48:39 +0100

    Fix checkstyle errors

--- client/src/main/java/client/scenes/NamePopupCtrl.java
+++ client/src/main/java/client/scenes/NamePopupCtrl.java
@@ -3,7 +3,6 @@ package client.scenes;
 import client.utils.NextScreen;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
-import commons.GameEntity;
 import commons.Player;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
