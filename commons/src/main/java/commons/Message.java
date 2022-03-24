681cf28279fffff2df5e78e202df44042ef82598
Jesse Vleeschdraager <J.C.Vleeschdraager@student.tudelft.nl>
2022-03-24 14:23:21 +0100

    Adds message class
    Adds a message class and corresponding test class containing 2 strings with the names of the emoji and the player for simplifying emoji communication.

---
+++ commons/src/main/java/commons/Message.java
@@ -0,0 +1,57 @@
+package commons;
+
+/**
+ * Class that represents an emoji-message that can be sent in multiplayer games.
+ */
+public class Message {
+
+  private String emojiName;
+  private String playerName;
+
+  /**
+   * The constructor for the class.
+   *
+   * @param emojiName  string that represents which emoji is being sent
+   * @param playerName the name of the player who sent the message
+   */
+  public Message(String emojiName, String playerName) {
+    this.emojiName = emojiName;
+    this.playerName = playerName;
+  }
+
+  /**
+   * Getter for the name of the emoji.
+   *
+   * @return the name of the emoji
+   */
+  public String getEmojiName() {
+    return emojiName;
+  }
+
+  /**
+   * Setter for the name of the emoji.
+   *
+   * @param emojiName the name of the emoji
+   */
+  public void setEmojiName(String emojiName) {
+    this.emojiName = emojiName;
+  }
+
+  /**
+   * Getter for the name of the player.
+   *
+   * @return the name of the player
+   */
+  public String getPlayerName() {
+    return playerName;
+  }
+
+  /**
+   * Setter for the name of the player.
+   *
+   * @param playerName the name of the player
+   */
+  public void setPlayerName(String playerName) {
+    this.playerName = playerName;
+  }
+}
