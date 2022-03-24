993e439d4faac2ca9d56275022a1fa5e6e629dd7
Adelina Cazacu <a.a.cazacu-1@student.tudelft.nl>
2022-03-24 18:19:26 +0100

    Add polling for start button

--- client/src/main/java/client/scenes/MPWaitingRoomCtrl.java
+++ client/src/main/java/client/scenes/MPWaitingRoomCtrl.java
@@ -2,6 +2,8 @@ package client.scenes;
 
 import client.utils.ServerUtils;
 import commons.GameEntity;
+import java.util.Timer;
+import java.util.TimerTask;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javax.inject.Inject;
@@ -27,14 +29,34 @@ public class MPWaitingRoomCtrl {
   public MPWaitingRoomCtrl(ServerUtils server, MainCtrl mainCtrl) {
     this.server = server;
     this.mainCtrl = mainCtrl;
+
+    new Timer().scheduleAtFixedRate(new TimerTask() {
+      /**
+       * Run method that gets executed once every second.
+       */
+      @Override
+      public void run() {
+        if (checkPlayerNo()) {
+          startButton.setVisible(true);
+        }
+      }
+    }, 0, 1000);
+  }
+
+  /**
+   * Method that checks whether the multiplayer game can be started.
+   *
+   * @return a boolean
+   */
+  public boolean checkPlayerNo() {
+    System.out.println(1);
+    return (server.getGame().getPlayers().size() > 1);
   }
 
   /**
    * Starts the game in multi-player mode.
    */
   public void startMultiPlayer() {
-    if (server.getGame().getPlayers().size() > 1) {
-      mainCtrl.showMoreExpensive(GameEntity.Type.MULTIPLAYER);
-    }
+    mainCtrl.showMoreExpensive(GameEntity.Type.MULTIPLAYER);
   }
 }
