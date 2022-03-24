2a6317efadebd8ebd673ceebe6a88a88719e1f9b
Adelina Cazacu <a.a.cazacu-1@student.tudelft.nl>
2022-03-24 12:36:15 +0100

    Fix pipeline

--- client/src/main/java/client/scenes/ChooseScreenCtrl.java
+++ client/src/main/java/client/scenes/ChooseScreenCtrl.java
@@ -3,6 +3,7 @@ package client.scenes;
 import client.utils.NextScreen;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
+import commons.GameEntity;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
