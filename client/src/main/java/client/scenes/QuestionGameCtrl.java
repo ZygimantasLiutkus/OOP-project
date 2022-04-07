78448889f6bc07e69264ab50273f6fd1f1910d98
Petar Koev <P.P.Koev@student.tudelft.nl>
2022-04-07 13:59:57 +0200

    Intermediate leaderboard after the 10th question
    Changing it from the test version to the game ready version

--- client/src/main/java/client/scenes/QuestionGameCtrl.java
+++ client/src/main/java/client/scenes/QuestionGameCtrl.java
@@ -666,7 +666,7 @@ public class QuestionGameCtrl {
    * Checks if the game type is single player and does the associated methods.
    */
   public void cooldownAnswer() {
-    if (questionNum == 1 && type.equals(GameEntity.Type.MULTIPLAYER)) {
+    if (questionNum == 10 && type.equals(GameEntity.Type.MULTIPLAYER)) {
       server.send("/app/messages", "intermediate");
     } else if (questionNum < 20) {
       nextQuestion();
